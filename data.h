d9f27a97617c6391ab9b410700c5ec5e64fa979a
lysu <sulifx@gmail.com>
2016-08-24 10:25:12 +0800

    start

---
+++ data.h
@@ -0,0 +1,6 @@
+#ifndef MEME_DATA_H
+#define MEME_DATA_H
+
+extern const char *progname;
+
+#endif //MEME_DATA_H
---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "data.h"
+
+
+int main(int argc, char *argv[]) {
+
+    progname = argv[0];
+
+    printf("%s", progname);
+
+    exit(EXIT_SUCCESS);
+}
